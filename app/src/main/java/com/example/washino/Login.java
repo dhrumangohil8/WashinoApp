e14d7065245f1d1acf56da74d80702e24699ce78
naman3523 <naman.doshi@somaiya.edu>
2020-05-26 00:58:26 +0530

    ui_updates

--- app/src/main/java/com/example/washino/Login.java
+++ app/src/main/java/com/example/washino/Login.java
@@ -2,15 +2,19 @@ package com.example.washino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
     Animation middleAnimation;
     TextView a;
+    Button login_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +23,13 @@ public class Login extends AppCompatActivity {
         a = findViewById(R.id.a);
         middleAnimation = AnimationUtils.loadAnimation(this, R.anim.middle_animation);
         a.setAnimation(middleAnimation);
+        login_button = findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), verifyotp.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/washino/Register.java
+++ app/src/main/java/com/example/washino/Register.java
@@ -2,22 +2,33 @@ package com.example.washino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
     Animation middleAnimation;
     TextView a;
+    Button register_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_register);
         a = findViewById(R.id.a);
+        register_button = findViewById(R.id.register_button);
         middleAnimation = AnimationUtils.loadAnimation(this, R.anim.middle_animation);
         a.setAnimation(middleAnimation);
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), verifyotp.class));
+            }
+        });
     }
 }
