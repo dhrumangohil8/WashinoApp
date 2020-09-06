cdcb83a27ef3f2db95799b05a39b0d876089d4d9
swatiabc <46042312+swatiabc@users.noreply.github.com>
2020-09-06 12:20:17 +0530

    Profile with firebase

---
+++ app/src/main/java/com/example/washino/User.java
@@ -0,0 +1,46 @@
+package com.example.washino;
+
+import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.List;
+
+public class User {
+
+    private String userName;
+    private String userGender;
+    private String userEmail;
+    private String userPhone;
+    private String userAddress;
+    private String userCarName;
+    private String userCarType;
+    private String userCarNumber;
+
+    public User() {
+
+    }
+
+    public User(List<String> list)
+    {
+        this.userName=list.get(0);
+        this.userGender=list.get(1);
+        this.userEmail=list.get(2);
+        this.userPhone=list.get(3);
+        this.userAddress=list.get(4);
+        this.userCarName=list.get(5);
+        this.userCarNumber=list.get(6);
+        this.userCarType=list.get(7);
+    }
+
+    public String getUserName(){return userName;}
+    public String getUserGender(){return userGender;}
+    public String getUserEmail(){return userEmail;}
+    public String getUserPhone(){return userPhone;}
+    public String getUserAddress(){return userAddress;}
+    public String getUserCarName(){return userCarName;}
+    public String getUserCarType(){return userCarType;}
+    public String getUserCarNumber(){return userCarNumber;}
+
+}
